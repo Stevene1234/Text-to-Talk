44be0c8af15a12e3211907d44ab9689bcba6ed95
Steven Eckler <114785247+Stevene1234@users.noreply.github.com>
2023-01-18 20:56:36 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ root.title("You text, I'll talk!")
 
 
 
+
 Msg = StringVar()
 Label(root, text = "You text, I'll talk!", font = 'arial 15 bold', bg = 'white smoke').pack()
 Label(root,text ='Enter Text', font = 'arial 15 bold', bg ='white smoke').place(x=20,y=60)
 entryfield = Entry(root, textvariable = Msg ,width ='50')
 entryfield.place(x=20,y=100)
 
-def Text_to_speech():
+def Text_to_talk():
+#converts text to talk
     Message = entryfield.get()
     speech = gTTS(text = Message)
     speech.save('project.mp3')
@@ -31,8 +33,8 @@ def Reset():
     Msg.set("")
 
 
-Button(root, text = "PLAY", font = 'arial 15 bold' , command = Text_to_speech ,width = '4').place(x=25,y=140)
-Button(root, font = 'arial 15 bold',text = 'EXIT', width = '4' , command = Exit, bg = 'OrangeRed1').place(x=100 , y = 140)
-Button(root, font = 'arial 15 bold',text = 'RESET', width = '6' , command = Reset).place(x=175 , y = 140)
+Button(root, text = "PLAY", font = 'arial 15 bold' , command = Text_to_talk ,width = '4', bg = 'lightgreen').place(x=25,y=140)
+Button(root, text = 'Exit', font = 'arial 15 bold', width = '4' , command = Exit, bg = 'red').place(x=100 , y = 140)
+Button(root, text = 'RESET',font = 'arial 15 bold', width = '6' , command = Reset).place(x=175 , y = 140)
 
 root.mainloop()
